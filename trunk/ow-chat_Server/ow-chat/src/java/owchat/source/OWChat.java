6bce2bd7ed98a385406ff71b50ba65aab7f55b9c
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-14 09:40:07 +0000

    2nd major import

---
+++ trunk/ow-chat_Server/ow-chat/src/java/owchat/source/OWChat.java
@@ -0,0 +1,136 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package owchat.source;
+
+import java.util.ArrayList;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+
+
+/**
+ *
+ * @author Hirantha Neranjan
+ */
+@WebService()
+public class OWChat {
+
+    ChatSystem chatSystem;
+
+    public OWChat() {
+        chatSystem = ChatSystem.initializeChatSystem();
+    }
+
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "registerUser")
+    public Boolean registerUser(@WebParam(name = "userName")
+    String userName, @WebParam(name = "password")
+    String password) throws OWChatException {
+        return chatSystem.registerUser(userName, password);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "signIn")
+    public String signIn(@WebParam(name = "userName")
+    String userName, @WebParam(name = "password")
+    String password) throws OWChatException {
+        return chatSystem.signIn(userName, password);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getAllUsers")      /////// Only for testing!!!
+    public java.lang.String[] getAllUsers() {
+        //TODO write your implementation code here:
+        return chatSystem.getAllUsers();
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "getAllFriends")      /////// Only for testing!!!
+    public java.lang.String[] getAllFriends(@WebParam(name = "keyString")
+            String keyString) {
+        //TODO write your implementation code here:
+        return chatSystem.getAllFriends(keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "resetChatServer")      /////// Only for testing!!!
+    public Boolean resetChatServer() {
+        return chatSystem.resetSystem();
+    }
+
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "signOut")
+    public Boolean signOut(@WebParam(name = "keyString")
+    String keyString) throws OWChatException {
+        return chatSystem.signOut(keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "addAsFriend")
+    public Boolean addAsFriend(@WebParam(name = "friendName")
+    String friendName, @WebParam(name = "keyString")
+    String keyString) throws OWChatException {
+        return chatSystem.addFriend(friendName, keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "sendMessage")
+    public Boolean sendMessage(@WebParam(name = "friendName")
+    String friendName, @WebParam(name = "message")
+    String message, @WebParam(name = "keyString")
+    String keyString) throws java.lang.Exception {
+        return chatSystem.sendMessage(friendName, message, keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "receiveMessages")
+    public owchat.source.Message[] receiveMessages(@WebParam(name = "keyString")
+    String keyString) throws OWChatException {
+        return chatSystem.receiveMessages(keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "unregisterUser")
+    public Boolean unregisterUser(@WebParam(name = "keyString")
+    String keyString) throws OWChatException {
+        return chatSystem.unregisterUser(keyString);
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "removeFromFriends")
+    public Boolean removeFromFriends(@WebParam(name = "friendName")
+    String friendName, @WebParam(name = "keyString")
+    String keyString) throws OWChatException {
+        return chatSystem.removeFriend(friendName, keyString);
+    }
+
+
+}
---
+++ trunk/ow-chat_Server/ow-chat/src/java/owchat/source/User.java
@@ -0,0 +1,143 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package owchat.source;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hirantha Neranjan
+ */
+public class User {
+
+    private String userName;
+    private String password;
+    private boolean online;
+    private String keyString;
+    private ArrayList<User> friends;
+    private ArrayList<Message> waitingMessages;
+
+    public User() {
+        online = false;
+        friends = new ArrayList<User>();
+        waitingMessages = new ArrayList<Message>();
+    }
+
+    public User(String userName, String pasword) {
+        this();
+        this.userName = userName;
+        this.password = pasword;
+    }
+
+    public boolean addFriend(User friend) {
+        if (isFriendsWtih(friend)) {
+            return false;
+        }
+        friends.add(friend);
+        return true;
+    }
+
+    public boolean removeFriend(User friend) {
+        if (isFriendsWtih(friend)) {
+            friends.remove(friend);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFriendsWtih(User friend) {
+        if (friends.indexOf(friend) < 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public ArrayList getFriends() {
+        return friends;
+    }
+
+    public boolean insertMessage(Message newMsg){
+        waitingMessages.add(newMsg);
+        return true;
+    }
+
+    public Message[] getWaitingMessages(){
+        Message[] msgs = new Message[waitingMessages.size()];
+        for (int i= 0; i < msgs.length; i++) {
+            msgs[i] = waitingMessages.get(i);
+        }
+        return msgs;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            if (((User) obj).userName.equals(this.userName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 37 * hash + (this.userName != null ? this.userName.hashCode() : 0);
+//        return hash;
+//    }
+    public String getKeyString() {
+        return keyString;
+    }
+
+    public String generateKeyString() {
+        keyString = "" + ( (userName.hashCode() * 5) + System.currentTimeMillis()
+                + (password.hashCode() * 3) )  + "@ow-chat";
+        return keyString;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the Online
+     */
+    public boolean isOnline() {
+        return online;
+    }
+
+    /**
+     * @param Online the Online to set
+     */
+    public void setOnline(boolean Online) {
+        this.online = Online;
+    }
+}
