367c18b28377d4bb93259137c76487b4b3b22df5
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-15 14:25:39 +0000

    MessageWindow fixed

---
+++ trunk/ow-chat_DotNetClient/OWChat/ChatClient/GlobalConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatClient
+{
+    static class GlobalConfig
+    {
+        //Name of the current user of the chat client
+        private static String displayName;
+
+        public static String DisplayName
+        {
+            get { return GlobalConfig.displayName; }
+            set { GlobalConfig.displayName = value; }
+        }
+
+        static GlobalConfig()
+        {
+
+        }
+
+
+
+
+    }
+}
