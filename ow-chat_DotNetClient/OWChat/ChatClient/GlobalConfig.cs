005647caabf595bd0a278e5e9bc8ee6f56571190
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-15 15:26:42 +0000

    ChatService references are added to this file

--- ow-chat_DotNetClient/OWChat/ChatClient/GlobalConfig.cs
+++ ow-chat_DotNetClient/OWChat/ChatClient/GlobalConfig.cs
@@ -9,6 +9,18 @@ namespace ChatClient
     {
         //Name of the current user of the chat client
         private static String displayName;
+        private static OWChatService.OWChatService chatService;
+
+        public static OWChatService.OWChatService ChatService
+        {
+            get 
+            {
+                if (chatService == null)
+                    chatService = new ChatClient.OWChatService.OWChatService();
+                return GlobalConfig.chatService; 
+            }
+            //set { GlobalConfig.chatService = value; }
+        }
 
         public static String DisplayName
         {
