69b3da6ec134a911ef78a4abe223cef29d9970b5
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 11:46:18 +0000

    Initial implementation of add friend

---
+++ ow-chat_DotNetClient/OWChat/ChatClient/AddFriendWindow.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ChatClient
+{
+    partial class frmAddFriendWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbUsers = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFriendName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(200, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(200, 90);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Available ow-chat users :";
+            // 
+            // lbUsers
+            // 
+            this.lbUsers.FormattingEnabled = true;
+            this.lbUsers.Location = new System.Drawing.Point(15, 149);
+            this.lbUsers.Name = "lbUsers";
+            this.lbUsers.Size = new System.Drawing.Size(260, 121);
+            this.lbUsers.TabIndex = 11;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(121, 50);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Enter friend\'s name :";
+            // 
+            // tbFriendName
+            // 
+            this.tbFriendName.Location = new System.Drawing.Point(130, 9);
+            this.tbFriendName.Name = "tbFriendName";
+            this.tbFriendName.Size = new System.Drawing.Size(145, 20);
+            this.tbFriendName.TabIndex = 8;
+            // 
+            // frmAddFriendWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 123);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbUsers);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFriendName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAddFriendWindow";
+            this.Text = "Add Friends";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbUsers;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFriendName;
+    }
+}
---
+++ ow-chat_DotNetClient/OWChat/ChatClient/AddFriendWindow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class frmAddFriendWindow : Form
+    {
+        public frmAddFriendWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.Height = 310;
+            MessageBox.Show("User searching not yet implemented");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (tbFriendName.Text.Trim() == "")
+            {
+                MessageBox.Show("Please enter your friends name.", "ow-chat", MessageBoxButtons.OK);
+                tbFriendName.Focus();
+            }
+            else
+            {
+                this.Text = this.Text + " : Please wait...";
+                this.Enabled = false;
+                bool result,gotRes;
+                try
+                {
+                    GlobalConfig.ChatService.addAsFriend(tbFriendName.Text, GlobalConfig.SessionKey, out result, out gotRes);
+                    if (result)
+                    {
+                        MessageBox.Show("Ow-chat user " + tbFriendName.Text + " added as a friend of you.", "ow-chat");
+                        GlobalConfig.mainWindow.userLoggedIn();
+                        this.Dispose();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error adding friend. please try again.", "ow-chat");
+                        this.Dispose();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Add Friend Error : "  + ex.Message, "ow-chat");
+                    this.Dispose();
+                }
+                
+
+
+            }
+        }
+    }
+}
