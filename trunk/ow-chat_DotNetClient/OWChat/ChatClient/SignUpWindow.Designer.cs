6bc34bfb993b27cc2eac1b4cbe7205f205795a5b
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 10:10:25 +0000

    signup errors fixed

--- trunk/ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.Designer.cs
+++ trunk/ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.Designer.cs
@@ -104,6 +104,7 @@
             this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // frmSignUpWindow
             // 
