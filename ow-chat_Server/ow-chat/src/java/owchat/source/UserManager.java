248fd511e11f3522269cf61142653f63e901dd54
farajfarook <farajfarook@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 12:55:10 +0000

    hiobernate + error

--- ow-chat_Server/ow-chat/src/java/owchat/source/UserManager.java
+++ ow-chat_Server/ow-chat/src/java/owchat/source/UserManager.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package owchat.source;
 
 import java.math.BigInteger;
@@ -10,159 +9,186 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author Hirantha Neranjan
  */
-public class UserManager
-{
+public class UserManager {
+
     private static ArrayList<User> users = new ArrayList<User>();
 
-    public static User GetUserByKeyString(String keyString){
-        for (User user : users) {
-            if (user.getKeyString().equals(keyString))
-                return user;
-        }
-        return null;
-    }
-
-    public static User GetUserByName(String userName){
-        for (User user : users) {
-            if (user.getUserName().equals(userName))
-                return user;
-        }
-        return null;
-//        User user = null;
-//        Session session = OwChatpersistantUtil.getSessionFactory().getCurrentSession();
-//        try
-//        {
-//            session.beginTransaction();
-//            user = (User)session.get(User.class,userName);
-//            session.getTransaction().commit();
-//        }
-//        finally
-//        {
-//            session.close();
-//            return user;
-//        }
-    }
-
-    public static void ClearUsers()
+    public static void LoginUser(User user)
     {
+        users.add(user);
+    }
+
+    public static void LogoutUser(User user)
+    {
+        users.remove(user);
+    }
+
+    public static User GetUserByKeyString(String keyString) {
+        for (User user : users) {
+            if (user.getKeyString().equals(keyString)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static User GetUserByName(String userName) {
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            User user = (User)session.load(User.class, userName);
+            tx.commit();
+            String uname = user.getUserName();
+            if(uname!="")
+                return user;
+            else
+                return null;
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void ClearUsers() {
         users = new ArrayList<User>();
     }
 
-    public static Boolean UserExcist(User newUser)
-    {
-//        for (int i = 0; i < users.size(); i++) {
-//            User user = users.get(i);
-//            if(user.equals(newUser)) return true;
-//        }
-//        return false;
-         return GetUserByName(newUser.getUserName())!=null;
+    public static Boolean UserExcist(User newUser) {
+        return GetUserByName(newUser.getUserName()) != null;
     }
 
-    public static Boolean AddUser(String username, String password)
-    {
+    public static Boolean AddUser(String username, String password) {
         User newUser = new User(username, GetMd5Digest(password));
-        if(UserExcist(newUser)) return false;
-        users.add(newUser);
-        return true;
-//        Session session = OwChatpersistantUtil.getSessionFactory().getCurrentSession();
-//        try
-//        {
-//            session.beginTransaction();
-//            session.save(newUser);
-//            session.getTransaction().commit();
-//            return true;
-//        }
-//        finally
-//        {
-//            session.close();
-//            return false;
-//        }
+        if (UserExcist(newUser)) {
+            return false;
+        }
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(newUser);
+            tx.commit();
+            return true;
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return false;
+        }
     }
 
-    public static Boolean RemoveUser(User newUser)
-    {
-        if(UserExcist(newUser)) return false;
-        users.remove(newUser);
-        return true;
-//        Session session = OwChatpersistantUtil.getSessionFactory().getCurrentSession();
-//        try
-//        {
-//            session.beginTransaction();
-//            session.delete(newUser);
-//            session.getTransaction().commit();
-//            return true;
-//        }
-//        finally
-//        {
-//            session.close();
-//            return false;
-//        }
+    public static Boolean RemoveUser(User newUser) {
+        if (UserExcist(newUser)) {
+            return false;
+        }
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.delete(newUser);
+            tx.commit();
+            return true;
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return false;
+        }
     }
 
     public static int UserCount()
     {
-        return users.size();
-//        Session session = OwChatpersistantUtil.getSessionFactory().getCurrentSession();
-//        try
-//        {
-//            session.beginTransaction();
-//            List list = session.createQuery("select u from user as u").list();
-//            session.getTransaction().commit();
-//            return list.size();
-//        }
-//        finally
-//        {
-//            session.close();
-//            return -1;
-//        }
-
-    }
-
-    public static User[] GetUsers()
-    {
-        return toUserArray(users);
-//        Session session = OwChatpersistantUtil.getSessionFactory().getCurrentSession();
-//        try
-//        {
-//            session.beginTransaction();
-//            List list = session.createQuery("select u from user as u").list();
-//            session.getTransaction().commit();
-//            User[] users = new User[list.size()];
-//            Iterator it = list.iterator();
-//            for (int i = 0; i < users.length; i++) {
-//                users[i] = (User) it.next();
-//            }
-//        }
-//        finally
-//        {
-//            session.close();
-//            return null;
-//        }
-    }
-
-    public static User GetByIndex(int index)
-    {
-        return users.get(index);
-    }
-
-    public static User[] Search(String searchContent)
-    {
-        ArrayList<User> matchedUsers = new ArrayList<User>();
-        for (Iterator<User> it = users.iterator(); it.hasNext();) {
-            User user = it.next();
-            if(user.getUserName().toLowerCase().indexOf(searchContent.toLowerCase())>=0)
-                matchedUsers.add(user);
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            int count = session.createQuery("select u from userdata as u").list().size();
+            tx.commit();
+            return count;
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return 0;
         }
-        return toUserArray(matchedUsers);
     }
 
-    private static User[] toUserArray(ArrayList<User> usersList)
-    {
+    public static User[] GetUsers() {
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List userList = session.createQuery("select u from userdata as u").list();
+            tx.commit();
+            return toUserArray(userList);
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return null;
+        }
+    }
+
+    public static User[] Search(String searchContent) {
+        SessionFactory sessionFactory = OwChatHibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            List userList = session.createQuery("select u from userdata as u where u.username like '%"+searchContent+"%'").list();
+            tx.commit();
+            return toUserArray(userList);
+        } catch (RuntimeException exception) {
+            if (tx != null && tx.isActive()) {
+                try {
+                    tx.rollback();
+                } catch (HibernateException e1) {
+                    throw exception;
+                }
+            }
+            return null;
+        }
+    }
+
+    private static User[] toUserArray(List<User> usersList) {
         Object[] userobjs = usersList.toArray();
         User[] userArr = new User[userobjs.length];
         for (int i = 0; i < userobjs.length; i++) {
@@ -172,17 +198,13 @@ public class UserManager
         return userArr;
     }
 
-    public static String GetMd5Digest(String input)
-    {
-        try
-        {
+    public static String GetMd5Digest(String input) {
+        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] messageDigest = md.digest(input.getBytes());
-            BigInteger number = new BigInteger(1,messageDigest);
+            BigInteger number = new BigInteger(1, messageDigest);
             return number.toString(16);
-        }
-        catch(NoSuchAlgorithmException e)
-        {
+        } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
