60e22d92e5ecc4721aeb371969c1b6168e0ba02e
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 12:41:09 +0000

    Minor modifications

--- ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.Designer.cs
+++ ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.Designer.cs
@@ -123,6 +123,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmSignUpWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ow-chat Sign Up";
             this.ResumeLayout(false);
             this.PerformLayout();
