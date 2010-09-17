6b96f3b250ab391bc410d6ee5c5dd72ca9ebca44
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 10:10:25 +0000

    signup errors fixed

--- ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.cs
+++ ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.cs
@@ -18,7 +18,72 @@ namespace ChatClient
 
         private void btnSignup_Click(object sender, EventArgs e)
         {
-            if (tbUserName.Text.Trim()=="") || (!tbPassword.Text.Equals(tbConfPassword.Text) || (tbConfPassword.Text.Trim()=="") ||
+            if ( (tbUserName.Text.Trim()=="") || 
+                (tbConfPassword.Text.Trim()=="") || 
+                (tbPassword.Text.Trim()=="") )
+            {
+                MessageBox.Show("User name or password cannot be empty.");
+                tbConfPassword.Text = "";
+                tbPassword.Text = "";
+                tbUserName.SelectAll();
+                tbUserName.Focus();
+                return;
+            }
+
+            if ((!tbPassword.Text.Equals(tbConfPassword.Text)))
+            {
+                MessageBox.Show("Password confirmation failed.");
+                tbPassword.Text = "";
+                tbConfPassword.Text = "";
+                tbPassword.Focus();
+                return;
+            }
+
+            this.Text = this.Text + " Please wait...";
+            bool result,gotRes;
+            try
+            {
+                GlobalConfig.ChatService.registerUser(tbUserName.Text, tbConfPassword.Text, out result, out gotRes);
+                //MessageBox.Show("@Return : " + result);
+                //MessageBox.Show("GotRes : " + gotRes);
+                if (!result)
+                {
+                    MessageBox.Show("User already registered.");
+                    tbPassword.Text = "";
+                    tbConfPassword.Text = "";
+                    tbUserName.SelectAll();
+                }
+                else
+                {
+                    String key = GlobalConfig.ChatService.signIn(tbUserName.Text, tbConfPassword.Text);
+                    if (key == null)
+                    {
+                        MessageBox.Show("Failed to SignIn. Please Try again");
+                        new frmSignInWindow().Show();
+                        this.Dispose();
+                    }
+                    else
+                    {
+                        GlobalConfig.SessionKey = key;
+                        GlobalConfig.DisplayName = tbUserName.Text;
+                        new frmMainWindow().Show();
+                        this.Dispose();
+                    }
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("SignUp Error : " + ex.Message);
+            }
+            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
