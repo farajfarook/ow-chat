af912a02294b29b8668bdf92d132a659adb7c4d3
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-17 09:04:12 +0000

    SignUpWindow created

---
+++ ow-chat_DotNetClient/OWChat/ChatClient/SignUpWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class frmSignUpWindow : Form
+    {
+        public frmSignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            if (tbUserName.Text.Trim()=="") || (!tbPassword.Text.Equals(tbConfPassword.Text) || (tbConfPassword.Text.Trim()=="") ||
+        }
+    }
+}
