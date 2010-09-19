251c4cb0e7dfe832eeac6171d822e7f3158e756f
hneranjank <hneranjank@379595e9-28d2-0786-46c4-2d8721698c53>
2010-09-19 14:11:50 +0000

    Sound play features added.

---
+++ trunk/ow-chat_DotNetClient/OWChat/ChatClient/SoundPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Media;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    static class OWChatSoundPlayer
+    {
+        static SoundPlayer buzzPlayer, signInPlayer, signOutPlayer, msgPlayer;
+        static bool doneLoading = false;
+
+        public static void loadSoundResources()
+        {
+            try
+            {
+                buzzPlayer = new SoundPlayer("c:\\Windows\\Media\\chimes.wav");
+                buzzPlayer.LoadAsync();
+                doneLoading = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in loading sounds : " + ex.Message, "ow-chat Sound System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+        }
+        public static void playBuzzSound()
+        {
+            if (doneLoading)
+            {
+                buzzPlayer.Play();
+            }
+        }
+    }
+}
